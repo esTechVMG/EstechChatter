7d9dba64ae4af93f8dd23b7240b5513b9b881359
Android C-512L <vincentc512l@gmail.com>
2020-06-15 17:59:40 +0200

    Preferences done (i think)

---
+++ app/src/main/java/estech/vmg/chatter/ChatActivity.java
@@ -0,0 +1,13 @@
+package estech.vmg.chatter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
