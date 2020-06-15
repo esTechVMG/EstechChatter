3b9cfca91ba0c72a0f596693ce1fc0953dc052da
Android C-512L <vincentc512l@gmail.com>
2020-06-15 10:06:55 +0200

    Preferences initial work

--- app/src/main/java/estech/vmg/chatter/ChatListActivity.java
+++ app/src/main/java/estech/vmg/chatter/ChatListActivity.java
@@ -3,8 +3,13 @@ package estech.vmg.chatter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -12,19 +17,22 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ChatListActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
+    FirebaseUser user;
     FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_list);
+        //get firebase instance
         mAuth=FirebaseAuth.getInstance();
-        FirebaseUser user= mAuth.getCurrentUser();
-        if(user==null){
-            Intent i=new Intent(this,LoginActivity.class);
-            startActivity(i);
-        }else{
+        //Firebase user check
+        if(isUserLoggedIn()){
+            //Load Chat List
             loadChatList();
+        }else{
+            //Open login
+            openLogin();
         }
     }
     public void loadChatList(){
@@ -34,14 +42,46 @@ public class ChatListActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.chatlist_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settings_menu_item:
+                //Open settings menu
+                Log.d("SettingsChatter","Opening settings");
+                openSettings();
+                break;
+            case R.id.signout_menu_item:
+                //Sign out user and open login again
+                mAuth.signOut();
+                openLogin();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
-        /*
-        FirebaseUser user = mAuth.getCurrentUser();
-        if(user==null){
-            finish();
-        }else{
+        if(isUserLoggedIn()){
             loadChatList();
-        }*/
+        }else{
+            finish();
+        }
+    }
+    private void openLogin(){
+        Intent i=new Intent(getBaseContext(),LoginActivity.class);
+        startActivity(i);
+    }
+    private void openSettings(){
+        Intent i=new Intent(getBaseContext(),SettingsActivity.class);
+        startActivity(i);
+    }
+    private boolean isUserLoggedIn(){
+        return mAuth.getCurrentUser()!=null;
     }
 }
